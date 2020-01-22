20851f06d2c7a84b964dc6e242aa21ecd7624f0a
David Pilger <dpilger26@gmail.com>
2020-01-21 23:06:31 -0700

    Started to fill out the netcdf4 image reader

--- scripts/ReadNetCDF4.py
+++ scripts/ReadNetCDF4.py
@@ -8,17 +8,36 @@ import matplotlib.pyplot as plt
 
 
 # ======================================================================================================================
-def readFile(filename: str, doPlot: bool = False):
+def readFile(filename: str, doPlot: bool = False) -> np.ndarray:
     """
     Reads the image data out of a netcdf4 file
     :arg: filename: the full filename to the .nc file
     :arg: doPlot: whether or not to plot the image
     :return: np.ndarray
     """
+    data = net.Dataset(filename=filename, mode='r')
+    
+    if 'Rad' not in data.variables:
+        raise RuntimeError(f'This netcdf4 file does not contain any image data.')
 
-
+    FILL_VALUE = 16383
+    image = np.array(data.variables['Rad'])
+    image[image == FILL_VALUE] = np.nan
 
     if doPlot:
-        plt.figure()
-        plt.imshow(image)
-        plt.grid(b=False)
+        # TODO - add timestamp to figure
+        imageMin = np.nanmin(image)
+        imageMax = np.nanmax(image)
+        ptp = imageMax - imageMin
+        cMax = ptp * 0.05 + imageMin
+
+        fig, ax = plt.subplots(1)
+        ax.imshow(X=image, clim=[imageMin, cMax])
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_yticklabels([])
+        ax.set_xticklabels([])
+        ax.grid(b=False)
+        plt.show()
+
+    return image
